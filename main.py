397b1e95b5a94a8c962174158891b0a28afb0461
JhoanDiazArdila <jhoan.diaz.ardila@hotmail.com>
2024-07-28 13:22:57 -0500

    feat: :construction: Nuevas lineas al final
    Se modifican al final para probar merge de ramas
    
    by:@JhoanDiaz

--- main.py
+++ main.py
@@ -3,3 +3,48 @@ print("este es un archivo de texto")
 print("este es otro archivo de texto")
 print("Otra prueba de merge")
 print("prueba otra vezz 🤐")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Estas son pruebas de commint en un mismos archivo")
+print("Estas lineas se haran merge")
+print("*"*200)
+e=2
+i=10
+b=e*i
+print(b)
