cf17a7aff11c888fddab7e05c57bb349c3d8ddd9
Velandia28 <luisfelipevll28@gmail.com>
2024-07-28 13:23:01 -0500

    feat: :beers: se agrego un conjunto de lineas
    Se crearon 4 lineas de codigo nuevas con mensajes en cada una de ellas
    
    by :@ Fv

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("este es un archivo de texto")
 print("este es otro archivo de texto")
 print("Otra prueba de merge")
 print("prueba otra vezz 🤐")
+
+print("EMPIZA LA PRUEBA DE MERGE ")
+print("HOLA SEAN BIENVENIDOS A  CAMPUSLAND")
+print("HOY HABLAREMOS DE PYTHON")
+print("Empezaremos con aclarando que python es un lenguaje de programacion de Alto nivel ")
+    
