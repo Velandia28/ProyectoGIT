37946c3a7ed110d6c2a1867895df8cf17859389c
Velandia28 <luisfelipevll28@gmail.com>
2024-07-28 13:31:19 -0500

    feat: :bug: se arreglo el main.py
    se agregaron nuevas lineas en el archivo
    
    by @ FV

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print("este es un archivo de texto")
 print("este es otro archivo de texto")
 print("Otra prueba de merge")
 print("prueba otra vezz 🤐")
+
+print("esto es una neuva prueba 😤😤")
+print("otra vezz pythin")
+print("Nos van  a filtrar Sebastian")
